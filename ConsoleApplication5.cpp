10c761c6487c485a5fb3326694e007e224ee9bcb
BuildTools <unconfigured@null.spigotmc.org>
2019-04-10 22:34:43 +1000

    no message

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -10,6 +10,8 @@
 #include <chrono>
 #include <tuple>
 #include <list>
+#include <vector>
+#include <algorithm>
 
 #define LOG(x)  std::cout << x << std::endl;
 using namespace std;
@@ -27,12 +29,26 @@ struct AccountStruct
 const int rowSize = 20;
 const int columnSize = 6;
 
+void printOptionsMenu() //display menu
+{
+	cout << "-----------------------------------------------------------------------------------\n";
+	cout << "1. Deposit Money\n";
+	cout << "2. Withdraw Money\n";
+	cout << "3. Transfer Money\n";
+	cout << "4. Show Transactions\n";
+	cout << "5. Open an Account\n";
+	cout << "6. Close an Account\n";
+	cout << "7. Sort Accounts from largest balance to smallest\n";
+	cout << "8. Sort Accounts in alphabetical order using the account name\n";
+	cout << "9. Quit Program\n";
+	cout << "-----------------------------------------------------------------------------------\n";
+}
+
 int getArrayIndexOfAccount(int accountNumber, struct AccountStruct accounts[rowSize][columnSize]) {
 	for (int i = 0; i < 20; i++)
 	{
 		if (accountNumber == accounts[i]->account)
 		{
-			LOG(i)
 			return i;
 		}
 	}
@@ -97,18 +113,25 @@ void depositMoney(int index, int amount, struct AccountStruct accounts[rowSize][
 			accounts[i]->balance += amount;
 			for (size_t i = 0; i < 6; i++)
 			{
-				outFile << accounts[i]->serial;
-				outFile << " ";
-				outFile << accounts[i]->account;
-				outFile << " ";
-				outFile << accounts[i]->firstName;
-				outFile << " ";
-				outFile << accounts[i]->lastName;
-				outFile << " ";
-				outFile << accounts[i]->balance;
-				outFile << " ";
-				outFile << accounts[i]->flag;
-				outFile << "\n";
+				if (accounts[i]->account != 0)
+				{
+					outFile << accounts[i]->serial;
+					outFile << " ";
+					outFile << accounts[i]->account;
+					outFile << " ";
+					outFile << accounts[i]->firstName;
+					outFile << " ";
+					outFile << accounts[i]->lastName;
+					outFile << " ";
+					outFile << accounts[i]->balance;
+					outFile << " ";
+					outFile << accounts[i]->flag;
+					outFile << "\n";
+				}
+				else
+				{
+					break;
+				}
 			}
 			outFile.close();
 		}
@@ -153,18 +176,25 @@ void withdrawMoney(int index, int amount, struct AccountStruct accounts[rowSize]
 		accounts[index]->balance -= amount;
 		for (size_t i = 0; i < rowSize; i++)
 		{
-			outFile << accounts[i]->serial;
-			outFile << " ";
-			outFile << accounts[i]->account;
-			outFile << " ";
-			outFile << accounts[i]->firstName;
-			outFile << " ";
-			outFile << accounts[i]->lastName;
-			outFile << " ";
-			outFile << accounts[i]->balance;
-			outFile << " ";
-			outFile << accounts[i]->flag;
-			outFile << "\n";
+			if (accounts[i]->account != 0)
+			{
+				outFile << accounts[i]->serial;
+				outFile << " ";
+				outFile << accounts[i]->account;
+				outFile << " ";
+				outFile << accounts[i]->firstName;
+				outFile << " ";
+				outFile << accounts[i]->lastName;
+				outFile << " ";
+				outFile << accounts[i]->balance;
+				outFile << " ";
+				outFile << accounts[i]->flag;
+				outFile << "\n";
+			}
+			else
+			{
+				break;
+			}
 		}
 		outFile.close();
 	}
@@ -200,11 +230,7 @@ void showTransactions(int index, struct AccountStruct account[rowSize][columnSiz
 	{
 		while (getline(inFile, line))
 		{
-			oldFileList.emplace_back(line);
-		}
-		for (auto const& i : oldFileList)
-		{
-			cout << i << endl;
+			LOG(line)
 		}
 		inFile.close();
 	}
@@ -277,25 +303,92 @@ void openAccount(struct AccountStruct account[rowSize][columnSize]) {
 	outFile.open(fileName + ".txt");
 	outFile << "You opened a new account with the balance of  " + to_string(balance) + " " + date;
 	outFile << "\n";
-
 	outFile.close();
-	/*outFile.open("list.txt");
+}
+
+void closeAccount(int index, struct AccountStruct account[rowSize][columnSize]) {
+	
+	if (account[index]->balance > 0)
+	{
+		account[index]->flag = 1;
+		ofstream outFile;
+		string line;
+		{
+			outFile.open("list.txt");
+			for (size_t i = 0; i < rowSize; i++)
+			{
+				if (account[i]->account != 0)
+				{
+					outFile << account[i]->serial;
+					outFile << " ";
+					outFile << account[i]->account;
+					outFile << " ";
+					outFile << account[i]->firstName;
+					outFile << " ";
+					outFile << account[i]->lastName;
+					outFile << " ";
+					outFile << account[i]->balance;
+					outFile << " ";
+					outFile << account[i]->flag;
+					outFile << "\n";
+				}
+				else
+				{
+					break;
+				}
+			}
+			outFile.close();
+		}
+	}
+	else
+	{
+		cerr << "Account has a balance greater than 0" << endl;
+	}
+}
+
+
+void sortBalances(struct AccountStruct accounts[rowSize][columnSize]) {
+	vector<AccountStruct>sortedAccount;
 	for (size_t i = 0; i < rowSize; i++)
 	{
-		outFile << account[i]->serial;
-		outFile << " ";
-		outFile << account[i]->account;
-		outFile << " ";
-		outFile << account[i]->firstName;
-		outFile << " ";
-		outFile << account[i]->lastName;
-		outFile << " ";
-		outFile << account[i]->balance;
-		outFile << " ";
-		outFile << account[i]->flag;
-		outFile << "\n";
+		if (accounts[i]->account != 0)
+		{
+			for (size_t j = 0; j < 1; j++)
+			{
+				sortedAccount.push_back(accounts[i][j]);
+			}
+		}
 	}
-	outFile.close();*/
+	sort(sortedAccount.begin(), sortedAccount.end(), [](const auto& lhs, const auto& rhs)
+	{
+		return	rhs.balance < lhs.balance;
+	});
+	for (auto i : sortedAccount)
+	{
+		cout << i.serial << " " << i.account << " " << i.firstName << " " << i.lastName << " " << i.balance << " " << i.flag << endl;
+	}
+}
+
+void sortNames(struct AccountStruct accounts[rowSize][columnSize]) {
+	vector<AccountStruct>sortedAccount;
+	for (size_t i = 0; i < rowSize; i++)
+	{
+		if (accounts[i]->account != 0)
+		{
+			for (size_t j = 0; j < 1; j++)
+			{
+				sortedAccount.push_back(accounts[i][j]);
+			}
+		}
+	}
+	sort(sortedAccount.begin(), sortedAccount.end(), [](const auto& lhs, const auto& rhs)
+	{
+		return lhs.firstName < rhs.firstName;
+	});
+	for (auto i : sortedAccount)
+	{
+		cout << i.serial << " " << i.account << " " << i.firstName << " " << i.lastName << " " << i.balance << " " << i.flag << endl;
+	}
 }
 
 int main()
@@ -328,13 +421,67 @@ int main()
 	inFile.close();
 	/* End Initial Setup */
 
-
+	printOptionsMenu();
+	int x, index, accountNumber, amount;
+	while (true)
+	{
+		cin >> x;
+		switch (x)
+		{
+		case 1:
+			cin >> accountNumber;
+			index = getArrayIndexOfAccount(accountNumber, account);
+			cin >> amount;
+			depositMoney(index, amount, account);
+			break;
+		case 2:
+			cin >> accountNumber;
+			index = getArrayIndexOfAccount(accountNumber, account);
+			cin >> amount;
+			withdrawMoney(index, amount, account);
+			break;
+		case 3:
+			cin >> accountNumber;
+			int secondAccount;
+			cin >> secondAccount;
+			index = getArrayIndexOfAccount(accountNumber, account);
+			cin >> amount;
+			transferMoney(accountNumber, secondAccount, amount, account);
+			break;
+		case 4:
+			cin >> accountNumber;
+			index = getArrayIndexOfAccount(accountNumber, account);
+			showTransactions(index, account);
+			break;
+		case 5:
+			openAccount(account);
+			break;
+		case 6:
+			cin >> accountNumber;
+			index = getArrayIndexOfAccount(accountNumber, account);
+			closeAccount(accountNumber, account);
+			break;
+		case 7:
+			sortBalances(account);
+			break;
+		case 8:
+			sortNames(account);
+			break;
+		default:
+			system("cls");
+			break;
+		}
+	}
+	
 	//getArrayIndexOfAccount(38531, account);
 
 	//depositMoney(3, 5000, account);
 	//withdrawMoney(3, 5000, account);
 	//transferMoney(3, 1, 5000, account);
-	//showTransactions(2, account);
-	openAccount(account);
+	//showTransactions(3, account);
+	//openAccount(account);
+	//closeAccount(3, account);
+	//sortBalances(account);
+	//sortNames(account);
 	system("pause");
  }
